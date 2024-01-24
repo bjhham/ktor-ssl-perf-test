85f5193ef5378d78e59c5d261890f6642f5734a8
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-01-24 10:06:28 +0100

    Several more options, added timeouts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.21"
-    id("io.ktor.plugin") version "2.3.7"
     application
 }
 
@@ -8,6 +7,7 @@ group = "io.ktor-test"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
@@ -15,18 +15,20 @@ application {
     mainClass = "io.ktor.perf.MainKt"
 }
 
+val ktor_version = "3.0.0-SNAPSHOT" // "2.3.7"
+
 dependencies {
 
-    implementation("io.ktor:ktor-network-tls-certificates")
-    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     // implementation("io.ktor:ktor-server-cio-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-server-call-logging")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-call-logging:$ktor_version")
     implementation("ch.qos.logback:logback-classic:1.4.12")
 
-    implementation("io.ktor:ktor-client-core-jvm")
-    implementation("io.ktor:ktor-client-cio")
-    implementation("io.ktor:ktor-client-apache5")
+    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-apache5:$ktor_version")
 
     testImplementation(kotlin("test"))
 }
--- src/main/kotlin/io/ktor/perf/Main.kt
+++ src/main/kotlin/io/ktor/perf/Main.kt
@@ -9,77 +9,50 @@ import io.ktor.http.*
 import io.ktor.network.tls.certificates.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.plugins.callloging.*
+import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.*
 import kotlinx.coroutines.*
-import org.slf4j.LoggerFactory
 import java.io.File
+import java.nio.charset.StandardCharsets
+import java.security.KeyStore
 import java.util.concurrent.atomic.AtomicInteger
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManagerFactory
 
-private const val PORT_NUMBER = 8843
+private const val SECURE = true
+private const val HTTP_PORT = 8888
+private const val HTTPS_PORT = 8843
 private const val SECRET = "pwd123"
 private const val KEY_ALIAS = "loadTest"
 private const val CLIENT_ENGINE = "cio"
-private const val NUMBER_OF_REQUESTS = 10_000
+private const val KEYSTORE_FILE = "build/keystore.jks"
+private const val NUMBER_OF_REQUESTS = 100_000
+private const val CONNECT_TIMEOUT: Long = 15_000
 
 @OptIn(DelicateCoroutinesApi::class)
 fun main() {
+    val sslConfig = buildSSLConfig()
+    val server = buildServer(sslConfig)
 
-    val keyStoreFile = File("build/keystore.jks")
-    val keyStore = generateCertificate(
-        file = keyStoreFile,
-        keyAlias = KEY_ALIAS,
-        keyPassword = SECRET,
-        jksPassword = SECRET,
-    )
+    server.start()
 
-    val environment = applicationEngineEnvironment {
-        log = LoggerFactory.getLogger("ktor.application")
-        sslConnector(
-            keyStore = keyStore,
-            keyAlias = KEY_ALIAS,
-            keyStorePassword = { SECRET.toCharArray() },
-            privateKeyPassword = { SECRET.toCharArray() }) {
-            port = PORT_NUMBER
-            keyStorePath = keyStoreFile
-        }
-        module(Application::module)
-    }
-
-    val server = embeddedServer(io.ktor.server.netty.Netty, environment).start()
-
-    val localSslContext = SSLContext.getInstance("TLS")
-    val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-    trustManagerFactory.init(keyStore)
-    localSslContext.init(null, trustManagerFactory?.trustManagers, null)
-
-    val client = when(CLIENT_ENGINE.uppercase()) {
-        "CIO" -> HttpClient(CIO) {
-            engine {
-                https {
-                    trustManager = trustManagerFactory?.trustManagers?.first()
-                }
-            }
-        }
-        "APACHE", "APACHE5" -> HttpClient(Apache5) {
-            engine {
-                sslContext = localSslContext
-            }
-        }
-        else -> throw IllegalStateException("Invalid engine $CLIENT_ENGINE")
-    }
+    val client = buildClient(sslConfig)
 
     val startTime = System.currentTimeMillis()
     val successfulRequests = AtomicInteger()
+    val (proto, port) = if (SECURE) "https" to HTTPS_PORT else "http" to HTTP_PORT
+    val requestUrl = "$proto://localhost:$port/hello"
+    server.application.log.info("Firing $NUMBER_OF_REQUESTS requests...")
+
     val job = GlobalScope.launch {
         for (i in 0 until NUMBER_OF_REQUESTS) {
             if (isActive) launch {
-                val response = client.get("https://localhost:$PORT_NUMBER/hello")
-                if (response.status.isSuccess() && response.bodyAsText() == "Hello, World!")
+                val response = client.get(requestUrl) {
+                    header(HttpHeaders.AcceptEncoding, StandardCharsets.UTF_8.displayName())
+                }
+                if (response.status.isSuccess() && response.bodyAsText() == "Hello, world!")
                     successfulRequests.getAndIncrement()
             }
         }
@@ -87,8 +60,8 @@ fun main() {
 
     runBlocking {
         job.join()
-        server.stop()
     }
+    server.stop()
 
     println(String.format(
         "%d of %d success in %.2f seconds",
@@ -98,11 +71,90 @@ fun main() {
     ))
 }
 
-fun Application.module() {
-    install(CallLogging)
-    routing {
-        get("/hello") {
-            call.respondText("Hello, World!", ContentType.Text.Plain)
+data class SSLConfig(
+    val keyStoreFile: File,
+    val keyStore: KeyStore,
+    val sslContext: SSLContext,
+    val trustManagerFactory: TrustManagerFactory,
+)
+
+private fun buildSSLConfig(): SSLConfig? {
+    if (!SECURE)
+        return null
+
+    val keyStoreFile = File(KEYSTORE_FILE)
+    val keyStore = generateCertificate(
+        file = keyStoreFile,
+        keyAlias = KEY_ALIAS,
+        keyPassword = SECRET,
+        jksPassword = SECRET,
+    )
+    val localSslContext = SSLContext.getInstance("TLS")
+    val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+    trustManagerFactory.init(keyStore)
+    localSslContext.init(null, trustManagerFactory?.trustManagers, null)
+
+    return SSLConfig(
+        keyStoreFile,
+        keyStore,
+        localSslContext,
+        trustManagerFactory,
+    )
+}
+
+private fun buildServer(
+    sslConfig: SSLConfig?
+) = embeddedServer(
+    Netty,
+    applicationProperties {
+        module {
+            routing {
+                get("/hello") {
+                    call.respondText("Hello, world!", ContentType.Text.Plain)
+                }
+            }
         }
     }
+) {
+    if (SECURE) {
+        sslConnector(
+            keyStore = sslConfig!!.keyStore,
+            keyAlias = KEY_ALIAS,
+            keyStorePassword = { SECRET.toCharArray() },
+            privateKeyPassword = { SECRET.toCharArray() }
+        ) {
+            port = HTTPS_PORT
+            keyStorePath = sslConfig.keyStoreFile.absoluteFile
+        }
+    } else {
+        connector {
+            port = HTTP_PORT
+        }
+    }
+}
+
+private fun buildClient(
+    sslConfig: SSLConfig?,
+) = when (CLIENT_ENGINE.uppercase()) {
+    "CIO" -> HttpClient(CIO) {
+        engine {
+            requestTimeout = CONNECT_TIMEOUT * 2
+            endpoint {
+                connectTimeout = CONNECT_TIMEOUT
+                keepAliveTime = CONNECT_TIMEOUT
+            }
+            https {
+                trustManager = sslConfig?.trustManagerFactory?.trustManagers?.first()
+            }
+        }
+    }
+
+    "APACHE", "APACHE5" -> HttpClient(Apache5) {
+        engine {
+            sslContext = sslConfig?.sslContext
+            connectTimeout = CONNECT_TIMEOUT
+        }
+    }
+
+    else -> throw IllegalStateException("Invalid engine $CLIENT_ENGINE")
 }
